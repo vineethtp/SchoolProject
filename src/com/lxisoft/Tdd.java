ee2cfbb3140c2b743546f58cda9b1e0431f966d0
vineethtp <75016468+vineethtp@users.noreply.github.com>
2020-11-25 17:28:56 +0530

    Update Tdd.java

--- src/com/lxisoft/Tdd.java
+++ src/com/lxisoft/Tdd.java
@@ -5,9 +5,9 @@ public class Tdd {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Welcone");
-		System.out.println("hai");
-		System.out.println("Good Morning");
+		System.out.println("School Project");
+		
+		
 	}
 
 }
